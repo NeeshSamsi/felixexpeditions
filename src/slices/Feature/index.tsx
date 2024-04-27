05fc689f95f202ba07453e5c4ea8df5022b6aaab
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-27 22:48:44 +0530

    feat: Responsive Features slcie

--- src/slices/Feature/index.tsx
+++ src/slices/Feature/index.tsx
@@ -1,5 +1,9 @@
-import { Content } from "@prismicio/client"
-import { SliceComponentProps } from "@prismicio/react"
+import { type Content } from "@prismicio/client"
+import { type SliceComponentProps } from "@prismicio/react"
+import dynamicIconImports from "lucide-react/dynamicIconImports"
+
+import SectionTitle from "@/components/SectionTitle"
+import Icon from "@/components/Icon"
 
 /**
  * Props for `Feature`.
@@ -15,7 +19,27 @@ const Feature = ({ slice }: FeatureProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for feature (variation: {slice.variation}) Slices
+      <SectionTitle
+        subheading={slice.primary.sub_heading}
+        heading={slice.primary.heading}
+      />
+
+      <div className="mt-8 grid gap-6 sm:grid-cols-2 sm:gap-8 md:grid-cols-3">
+        {slice.items.map(({ icon, title, description }, index) => (
+          <div key={index} className="space-y-3">
+            <h3 className="flex items-center gap-4 text-xl font-medium lg:text-2xl 2xl:text-3xl">
+              <Icon
+                name={icon as keyof typeof dynamicIconImports}
+                className="size-7 2xl:size-8"
+              />
+              <span>{title}</span>
+            </h3>
+            <p className="max-w-[45ch] text-base font-light lg:text-lg 2xl:text-xl">
+              {description}
+            </p>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
