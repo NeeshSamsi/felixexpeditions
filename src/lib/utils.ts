966b2eaa2c5d7f883676a3693411a211b54a956e
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-28 13:45:23 +0530

    feat: Add date formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type DateStyle = Intl.DateTimeFormatOptions["dateStyle"]
+
+export function formatDate(date: Date, dateStyle: DateStyle) {
+  const formatter = new Intl.DateTimeFormat("en", { dateStyle })
+
+  return formatter.format(date)
+}
